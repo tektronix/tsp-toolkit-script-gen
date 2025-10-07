9c5543bb8cf2cb3a2aecf1c2c4b61559b9d8df8e
jharajeev55 <jharajeev55@gmail.com>
2025-10-07 12:06:26 +0530

    Handle list checkbox operations (#20)
    When List checkbox is checked disable start, stop and style and enable
    Edit and
    when its unchecked enable start, stop and style and disable Edit

--- script-gen-ui/src/app/components/main-sweep/sweep/sweep.component.ts
+++ script-gen-ui/src/app/components/main-sweep/sweep/sweep.component.ts
@@ -66,6 +66,7 @@ export class SweepComponent implements OnChanges {
   @Input() sweepChannel: SweepChannel | undefined;
   @Input() isSweepExpanded = false;
   @Input() deviceList: Device[] = [];
+  @Input() isListSweep = false;
   @Output() emitSweepExpanderState = new EventEmitter<{
     uuid: string;
     isExpanded: boolean;

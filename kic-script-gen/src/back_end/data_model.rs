5753a0b0fa070fc1ad0c6961974effbe1505f37e
pavannarayana <reachpavann@gmail.com>
2025-11-04 16:55:58 +0530

    Track source limit

--- kic-script-gen/src/back_end/data_model.rs
+++ kic-script-gen/src/back_end/data_model.rs
@@ -93,7 +93,7 @@ impl DataModel {
     pub fn process_data_from_saved_config(&mut self, data: String) -> String {
         match serde_json::from_str::<SweepModel>(&data) {
             Ok(mut sweep_model) => {
-                println!("Successfully deserialized saved JSON in server: {sweep_model:?}");
+                //println!("Successfully deserialized saved JSON in server: {sweep_model:?}");
                 sweep_model.sweep_config.evaluate();
 
                 self.sweep_model = sweep_model.clone();
